1fab0bbfec6d74fbe1e683244646191728ec4ea9
Vishal <vishalmhjn3@gmail.com>
2022-12-08 20:34:36 +0100

    Work on documentation

--- src/generate_additional.py
+++ src/generate_additional.py
@@ -1,96 +1,77 @@
-# coding=utf-8
 # To create sumo detector file : addition.add.xml
 import pandas as pd
-import read_class
 import sumo_class
 from tqdm import tqdm
-import sys
 
 if __name__=="__main__":
 
-	scenario=sys.argv[1]
-	
-	DETECTOR_POS = -20
+    # location of the detector from the node
+    DETECTOR_POS = -20
 
-	if scenario=="munich":
-		NETWORK_CSV = "../scenario_munich/network.net.csv"
-		DETECTOR_MAPPING = "../sharedstreets/detector_sumo_mapping_munich.csv"
-		FILE = "../data/BAST/day_counts_190605.csv"
-		# FILTER = "2020-10-07" # 19:00:00"
-		TIME_COL = "Stunde"
-		PATH_NETWORK_CSV = NETWORK_CSV
-		PATH_EDGE_CSV = "../scenario_munich/network.edg.csv"
-		SUMO_COLUMN = 'sumo_id'
-		DETECTOR_COLUMN = 'munich_id'
-		DETECTOR_LATITUDE = 'detector_lat'
-		DETECTOR_LONGITUDE = 'detector_lon'
-		
-		paris = pd.read_csv(FILE)
-		# df = paris.read_file(FILTER, time_col=TIME_COL)
-		# df.dropna(inplace=True)
-		# df.reset_index(inplace=True)
-		# filtered output for only keeping sensors where data is available
-		OUTPUT = "../scenario_munich/addition_shared_streets_bast.add.xml"
-		# Renaming the columns of the current time data as they are
-		# different from the archived data
+    # path to the SUMO network in CSV format. Use xml2csv tool to generate this
+    NETWORK_CSV = "../sample_files/network.net.csv"
+    # path to the output from merge.py which contains mapping of detectors to OSM edges
+    DETECTOR_MAPPING = "../sharedstreets_outputs/point_edge_mapping.csv"
+    # path to the network edge file
+    PATH_EDGE_CSV = "../sample_files/network.edg.csv"
 
-	else:
-		raise ValueError
+    # column ids from DETECTOR_MAPPING file
+    SUMO_COLUMN = 'sumo_id'
+    DETECTOR_COLUMN = 'munich_id'
+    DETECTOR_LATITUDE = 'detector_lat'
+    DETECTOR_LONGITUDE = 'detector_lon'
 
+    # path to the output/ SUMO detector file
+    OUTPUT = "../sample_files/additional.add.xml"
 
-	detector_locations = pd.read_csv(DETECTOR_MAPPING)
+    detector_locations = pd.read_csv(DETECTOR_MAPPING)
+    temp = detector_locations
 
+    # we need edge file, to know the number of lanes in addition.add.xml
+    # get the length and num lanes of the edges
+    sumo_obj = sumo_class.Sumo_Network(NETWORK_CSV, PATH_EDGE_CSV)
+    df_edge_length, df_edge_lanes = sumo_obj.write_edges()
 
-	temp = detector_locations
+    # start wrtiting the additional file
+    a = """<additional>""" 
+    b = ""
+    check_list_sumo_edge = []
+    check_list_id = []
+    temp[DETECTOR_COLUMN] = temp.apply(lambda x: str(x[DETECTOR_COLUMN])+"_"+str(int(x.direction)), axis=1)
+    temp.drop_duplicates(subset=[DETECTOR_COLUMN], inplace=True)
+    for i, j in tqdm(enumerate(zip(temp[SUMO_COLUMN], temp[DETECTOR_COLUMN], temp[DETECTOR_LATITUDE], temp[DETECTOR_LONGITUDE]))):
+        try:
+            # try except added to cater the cases when some of the detector links are not in the matched
+            # sumo network. but there are many links for one detector.
+            len_lane = float(df_edge_length[df_edge_length.edge_id==j[0]]['lane_length'])
+            if j[0] in check_list_sumo_edge:
+                # only one detector on one sumo edge
+                pass
+            elif j[1] in check_list_id:
+                # only one detector with one name
+                pass
+            ## insufficient length of the lane for detector
+            elif len_lane < DETECTOR_POS:
+                pass
+            else:
+                temp_len = len(df_edge_lanes[df_edge_lanes.edge_id==j[0]])
+                if temp_len==1:
+                    n_lanes = int(df_edge_lanes[df_edge_lanes.edge_id==j[0]]["edge_numLanes"])
+                else:
+                    n_lanes = list((df_edge_lanes[df_edge_lanes.edge_id==j[0]]["edge_numLanes"]).astype(int))[0]
+                for k in range(0, n_lanes):
+                    b = b+"""<e1Detector file="out.xml" freq="900.0" id="""+"\""+"e1Detector_"+j[0]+"_"+str(k)+"\""+" lane="+"\""+j[0]+"_"+str(k)+"\""+""" pos="""+"\""+str(DETECTOR_POS)+"\""+""" name="""+"\""+str(j[1])+"\""+"""><param key="C" value="""+"\""+str(j[2])+", "+str(j[3])+"\""+"""/></e1Detector>"""
+                check_list_sumo_edge.append(j[0])
+                check_list_id.append(j[1])
+        except Exception as e:
 
-	# we need edge file, to know the number of lanes in addition.add.xml
-	# get the length and num lanes of the edges
-	sumo_obj = sumo_class.Sumo_Network(PATH_NETWORK_CSV, PATH_EDGE_CSV)
-	df_edge_length, df_edge_lanes = sumo_obj.write_edges()
+            print(e)
+            pass
+            
+    c = """</additional>"""
+    file_text = a+b+c
+    # stop wrtiting the additional file
 
-
-	a = """<additional>""" 
-	b = ""
-	check_list_sumo_edge = []
-	check_list_id = []
-	if scenario=="munich":
-		temp[DETECTOR_COLUMN] = temp.apply(lambda x: str(x[DETECTOR_COLUMN])+"_"+str(int(x.direction)), axis=1)
-		temp.drop_duplicates(subset=[DETECTOR_COLUMN], inplace=True)
-	for i, j in tqdm(enumerate(zip(temp[SUMO_COLUMN], temp[DETECTOR_COLUMN], temp[DETECTOR_LATITUDE], temp[DETECTOR_LONGITUDE]))):
-		# print(df_edge_length[df_edge_length.edge_id==j[0]])
-		try:
-			# try except added to cater the cases when some of the detector links are not in the matched
-			# sumo network. but there are many links for one detector.
-			len_lane = float(df_edge_length[df_edge_length.edge_id==j[0]]['lane_length'])
-			if j[0] in check_list_sumo_edge:
-				# only one detector on one sumo edge
-				pass
-			elif j[1] in check_list_id:
-				# only one detector with one name
-				pass
-			## insufficient length of the lane for detector
-			elif len_lane < DETECTOR_POS:
-				pass
-			else:
-				# print(len_lane)
-				# pass
-				temp_len = len(df_edge_lanes[df_edge_lanes.edge_id==j[0]])
-				if temp_len==1:
-					n_lanes = int(df_edge_lanes[df_edge_lanes.edge_id==j[0]]["edge_numLanes"])
-				else:
-					n_lanes = list((df_edge_lanes[df_edge_lanes.edge_id==j[0]]["edge_numLanes"]).astype(int))[0]
-				for k in range(0, n_lanes):
-					b = b+"""<e1Detector file="out.xml" freq="900.0" id="""+"\""+"e1Detector_"+j[0]+"_"+str(k)+"\""+" lane="+"\""+j[0]+"_"+str(k)+"\""+""" pos="""+"\""+str(DETECTOR_POS)+"\""+""" name="""+"\""+str(j[1])+"\""+"""><param key="C" value="""+"\""+str(j[2])+", "+str(j[3])+"\""+"""/></e1Detector>"""
-				check_list_sumo_edge.append(j[0])
-				check_list_id.append(j[1])
-		except Exception as e:
-
-			print(e)
-			pass
-			
-	c = """</additional>"""
-	file_text = a+b+c
-	
-	with open(OUTPUT, "w") as f:
-		f.write(file_text)
-		f.close()
+    with open(OUTPUT, "w") as f:
+        f.write(file_text)
+        f.close()
--- src/shared_street.sh
+++ src/shared_street.sh
@@ -3,9 +3,9 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm use 10
 
-export SUMO_NETWORK_GEOJSON=../scenario_munich/sumo_network.geojson
-export SUMO_SS=../sharedstreets/sumo_network_munich.geojson
-export DETECTORS_GEOJSON=../scenario_munich/bast_munich.geojson
-export DETECTORS_SS=../sharedstreets/bast_munich.geojson
+export SUMO_NETWORK_GEOJSON=../sample_files/edgefile.geojson
+export SUMO_SS=../sharedstreets_outputs/edgefile.geojson
+export DETECTORS_GEOJSON=../sample_files/pointfile.geojson
+export DETECTORS_SS=../sharedstreets_outputs/pointfile.geojson
 
 shst match $DETECTORS_GEOJSON --out=$DETECTORS_SS --snap-intersections --follow-line-direction --follow-line-direction --snap-intersections --search-radius=100 --offset-line=1 
